8a357189309428a136201734e0585fdb1a12845f
mdnumanahmed <mnumansyl@gmail.com>
2023-02-26 03:07:02 +0600

    module 34 practice complated

--- Module-34-API-Example-EDGE-Case/js/quote.js
+++ Module-34-API-Example-EDGE-Case/js/quote.js
@@ -1,1 +1,14 @@
-console.log('quote js');
+
+const loadQuote = () => {
+    fetch('https://api.kanye.rest')
+    .then(res => res.json())
+    .then(data => displayQuote(data))
+}
+
+const displayQuote = (quote) => {
+    const quoteContainer = document.getElementById('quote')  
+    // show one randomly
+    quoteContainer.innerHTML = quote.quote
+
+}
+loadQuote()
